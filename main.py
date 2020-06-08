f48dc0cc2a52b8ad50e56ff7eec20b19f291fca8
Rhydian <rhydian98@gmail.com>
2020-06-08 17:55:01 +0100

    Beginnings Of website

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+@app.route('/post_id')
+def post():
+    postName
+    return render_template('post.html', postName=postName)
+@app.route('/create_post')
+def create_post():
+    return render_template('makePost.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
