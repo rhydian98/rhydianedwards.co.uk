6b1892f54e97e163117f48fd3d88971e78750f24
rhydian98 <rhydian98@gmail.com>
2020-06-11 13:54:39 +0100

    Beginning work on database system for Posts.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/E/Programming/PythonProjects/Flask Tutorial/rhydianedwards.co.uk/app.db'
+app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
-class post(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(20))
-    datePosted = db.Column(db.DateTime)
-    content = db.Column(db.Text) 
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
@@ -18,21 +16,13 @@ def index():
 def make_post():
     return render_template('post.html')
 
-
-
-@app.route('/makePost', methods=['POST'])
-def make_post():
-    return render_template('makePost.html')
-
-@app.route('/create_post',methods= ['POST'])
+@app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
-    title = request.form['postTitle']
-    tags = request.form['tags']
-    content = request.form['postContent']
-    return '<h1>Title: {} tags: {} Content: {}'.format(title, tags, content)
+    return render_template('makePost.html')
+    
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
